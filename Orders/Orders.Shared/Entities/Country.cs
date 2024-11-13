7da9d61354b077cd810ebb392abb41b6c6ba921e
gonzajaimes <gonzaiames@hotmail.com>
2024-11-12 21:59:21 -0500

    Create database with first entity Countries

---
+++ Orders/Orders.Shared/Entities/Country.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "País")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null!;
+    }
+}
